8e53378c0209813263f5c00eab699bcb22179f7c
Jacob Hausmann <jeh7497@gmail.com>
2020-06-02 12:00:44 -0500

    Initial Commit

---
+++ FlightAtlasModel/FlightAtlasModel.h
@@ -0,0 +1,13 @@
+//
+//  FlightAtlasModel.h
+//  FlightAtlasModel
+//
+//  Created by Jacob Hausmann on 6/2/20.
+//  Copyright © 2020 Jacob Hausmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlightAtlasModel : NSObject
+
+@end
---
+++ FlightAtlasModel/FlightAtlasModel.m
@@ -0,0 +1,13 @@
+//
+//  FlightAtlasModel.m
+//  FlightAtlasModel
+//
+//  Created by Jacob Hausmann on 6/2/20.
+//  Copyright © 2020 Jacob Hausmann. All rights reserved.
+//
+
+#import "FlightAtlasModel.h"
+
+@implementation FlightAtlasModel
+
+@end
